c7f006be5abe0922a926c609432dc0ed02b8ebca
Paulo Barbosa <paulo_total10@hotmail.com>
2014-09-05 12:37:48 -0300

    Dando continuidade as aulas de Angular.

--- start_angular/js/app.js
+++ start_angular/js/app.js
@@ -2,4 +2,13 @@
   'use strict';
   var app = angular.module('startAngular', ['startController']);
 
+  app.run(['$rootScope', function($rootScope){
+  	$rootScope.usuarios = [
+  		{nome: 'Pauloooo', matricula: 90292892},
+		{nome: 'João', matricula: 938938393},
+		{nome: 'Maria', matricula: 62683638},
+		{nome: 'José', matricula: 727292773}
+  	];
+  }]);
+
 })();
--- start_angular/js/controllers.js
+++ start_angular/js/controllers.js
@@ -1,14 +1,27 @@
 var meusControllers = angular.module('startController', []);
 
-meusControllers.controller('IndexController', ['$scope', function($scope){
-	$scope.descricao = '';
+meusControllers.controller('IndexController',
+	['$scope', 
+		function($scope){
 
-	$scope.minimoDeCaracteres = function(){
-		return $scope.descricao.length < 10;
-	}
+			$scope.descricao = '';
 
-	$scope.alertar = function(){
-		alert('pegando!');
-	}
+			$scope.minimoDeCaracteres = function(){
+				return $scope.descricao.length < 10;
+			}
+
+			$scope.alertar = function(){
+				alert('pegando!');
+			}
+
+}]);
+
+meusControllers.controller('IndexController2',
+	['$scope',
+		function($scope){
+
+			$scope.calcular = function(){
+				$scope.media = (parseInt($scope.a) + parseInt($scope.b)) / 2;
+			}
 
 }]);
